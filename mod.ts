4cba6b856e0c7316a0fba367ed8935d208ba31ce
DjDeveloperr <djsandhu2005@gmail.com>
2021-09-12 13:48:02 +0530

    add some things

--- mod.ts
+++ mod.ts
@@ -1,17 +1,123 @@
-let path = ""
+let path
 if (Deno.build.os === "linux") {
   path = "./target/debug/libdeno_minifb.so"
 } else if (Deno.build.os === "windows") {
   path = "./target/debug/deno_minifb.dll"
 } else if (Deno.build.os === "darwin") {
   path = "./target/debug/libdeno_minifb.dylib"
-}
+} else throw new Error(`Unsupported OS: ${Deno.build.os}`)
 
-const dylib = Deno.dlopen(path, {
+const lib = Deno.dlopen(path, {
+  window_new: {
+    parameters: ["buffer", "usize", "usize"],
+    result: "u32",
+  },
+
+  window_close: {
+    parameters: ["u32"],
+    result: "u32",
+  },
+
+  window_is_open: {
+    parameters: ["u32"],
+    result: "u32",
+  },
+
+  window_is_active: {
+    parameters: ["u32"],
+    result: "u32",
+  },
+
+  window_limit_update_rate: {
+    parameters: ["u32", "u64"],
+    result: "u32",
+  },
+
+  window_update_with_buffer: {
+    parameters: ["u32", "buffer", "usize", "usize"],
+    result: "u32",
+  },
+
+  window_update: {
+    parameters: ["u32"],
+    result: "u32",
+  },
 })
 
-function show(title:string, width=800, height=600) {
+const encode = (str: string): Uint8Array => (Deno as any).core.encode(str)
 
+function unwrap(result: number) {
+  if (result !== 0) {
+    throw new Error(`Non-Zero Value Returned: ${result}`)
+  }
 }
 
-export default show
+export class Window {
+  #id
+
+  constructor(
+    title: string,
+    public width: number,
+    public height: number
+  ) {
+    this.#id = lib.symbols.window_new(
+      new Uint8Array([...encode(title), 0]),
+      width,
+      height
+    )
+  }
+
+  /**
+   * Limits the Window's update rate to the given number of microeconds, or 0 to disable.
+   * 
+   * @param micros Microseconds to limit the update rate to
+   */
+  limitUpdateRate(micros: number) {
+    unwrap(lib.symbols.window_limit_update_rate(this.#id, micros) as number)
+  }
+
+  /**
+   * Returns true if the Window is open.
+   */
+  get isOpen() {
+    return lib.symbols.window_is_open(this.#id) === 1
+  }
+
+  /**
+   * Returns true if the Window is active.
+   */
+  get isActive() {
+    return lib.symbols.window_is_active(this.#id) === 1
+  }
+
+  /**
+   * 
+   * @param buffer Frame Buffer containing RGBA data
+   * @param width Width of the buffer, defaults to the Window's width
+   * @param height Height of the buffer, defaults to the Window's height
+   */
+  updateWithBuffer(buffer: Uint8Array, width?: number, height?: number) {
+    unwrap(
+      lib.symbols.window_update_with_buffer(
+        this.#id,
+        buffer,
+        width ?? this.width,
+        height ?? this.height
+      ) as number
+    )
+  }
+
+  /**
+   * Updates the window without drawing a buffer, mostly event handlers related.
+   */
+  update() {
+    unwrap(lib.symbols.window_update(this.#id) as number)
+  }
+
+  /**
+   * Closes (and deletes internal reference of) the Window.
+   */
+  close() {
+    unwrap(lib.symbols.window_close(this.#id) as number)
+  }
+}
--- test.ts
+++ test.ts
@@ -1,11 +1,5 @@
 import { assertEquals } from "https://deno.land/std@0.106.0/testing/asserts.ts"
-import minifb from './mod.ts'
 
 Deno.test("has tests", () => {
   assertEquals(true, !false)
 })
-
-Deno.test("has correct shap", () => {
-  console.log(minifb)
-})
-
