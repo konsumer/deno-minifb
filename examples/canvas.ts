c89ac60e279cddce6db6f4bbd38ec2a1a10da806
David Konsumer <konsumer@jetboystudio.com>
2021-09-12 03:16:44 -0700

    move basic demo to canvas

--- examples/canvas.ts
+++ examples/canvas.ts
@@ -1,1 +1,39 @@
-// TODO: make example of using deno canvas
+import { MiniFB } from "../mod.ts"
+import { createCanvas } from "https://raw.githubusercontent.com/DjDeveloperr/deno-canvas/dc0969b15f1e30a866a6bf6bed8b8a4b7393f925/mod.ts"
+
+const width = 500, height = 500
+
+const canvas = createCanvas(width, height)
+const ctx = canvas.getContext("2d")
+const win = new MiniFB("My Window", width, height)
+
+// https://stackoverflow.com/a/45140101/12101923
+function strokeStar(x: number, y: number, r: number, n: number, inset: number) {
+  ctx.save()
+  ctx.beginPath()
+  ctx.translate(x, y)
+  ctx.moveTo(0, 0 - r)
+  for (let i = 0; i < n; i++) {
+    ctx.rotate(Math.PI / n)
+    ctx.lineTo(0, 0 - (r * inset))
+    ctx.rotate(Math.PI / n)
+    ctx.lineTo(0, 0 - r)
+  }
+  ctx.closePath()
+  ctx.fill()
+  ctx.restore()
+}
+
+ctx.fillStyle = 'yellow'
+const r = (width - 20) / 2
+strokeStar(r + 10, r + 10, r, 5, 0.5)
+
+win.updateWithBuffer(canvas.getRawBuffer(0, 0, width, height))
+
+setInterval(() => {
+  if (win.open) {
+    win.update()
+  } else {
+    Deno.exit(0)
+  }
+}, 1000 / 60)
