53210291ef3c2777a9018fe89909f1695bb892c4
David Konsumer <konsumer@jetboystudio.com>
2021-09-14 22:54:29 -0700

    simplifying cross demo

--- examples/canvas_cross.js
+++ examples/canvas_cross.js
@@ -1,16 +1,78 @@
 // this demonstrates using the same code in a browser or in deno
-// normally you would probly import just the function that operates on canvas or whatever
+// normally you would probly import just the function that operates on canvas, and keep the platform-specfic code in a wrapper
 // but I waanted to keep it all self-contained in a single file
+
+/* global Image, Deno */
+
+// this is a compatability layer
+class CanvasUtils {
+  // load more libs, if you're in deno
+  constructor () {
+    if ('Deno' in window) {
+      this.libs = Promise.all([
+        import('https://raw.githubusercontent.com/DjDeveloperr/deno-canvas/master/mod.ts'),
+        import('../mod.ts')
+      ])
+        .then(([canvas, minifb]) => ({ canvas, minifb }))
+    }
+  }
+
+  // return a canvas
+  async canvas (width, height) {
+    if ('Deno' in window) {
+      const libs = await this.libs
+      this._win = new libs.minifb.MiniFB('minifb!', width, height)
+      return libs.canvas.createCanvas(width, height)
+    } else {
+      const c = document.querySelector('canvas')
+      c.width = width
+      c.height = height
+      return c
+    }
+  }
+
+  // return an image
+  async image (src) {
+    if ('Deno' in window) {
+      const libs = await this.libs
+      return await libs.canvas.loadImage(src)
+    } else {
+      return new Promise((resolve, reject) => {
+        const image = new Image()
+        image.onload = () => resolve(image)
+        image.onerror = reject
+        image.src = src
+      })
+    }
+  }
+
+  // setup main loop
+  async loop (cb, canvas) {
+    if ('Deno' in window) {
+      setInterval(() => {
+        if (this._win.open) {
+          cb(canvas)
+          this._win.updateWithBuffer(canvas.getRawBuffer(0, 0, canvas.width, canvas.height))
+        } else {
+          Deno.exit(0)
+        }
+      }, 1000 / 60)
+    } else {
+      const frame = () => {
+        cb(canvas)
+        window.requestAnimationFrame(frame)
+      }
+      window.requestAnimationFrame(frame)
+    }
+  }
+}
+
+// everythign below here is same code, regardless of platform
 
 // this is a spritesheet animation-manager
 class Animation {
   constructor (image, frames = [0], speed = 1000, width = 32, height = 32) {
-    if (typeof image === 'string') {
-      this.image = new Image()
-      this.image.src = image
-    } else {
-      this.image = image
-    }
+    this.image = image
     this.playing = true
     this.speed = speed
     this.width = width
@@ -33,66 +95,13 @@ class Animation {
 
 const spritesheet = 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAYAAAAAYAgMAAACO68K2AAAACVBMVEUAAADk5+P///9QNmNfAAAA6UlEQVRIx7WTMQ4DIQwEl4gqFfWVqaJ7BXwnT6E6vhCliV+ZNaYguhQpTDOyxkhrLEBAQo7GHJECjF4eJaZURsNqa3h553nPHr7znj1W7H32WLH32TMNlo+ejzCMl0e4yIv5JPNJ5vfayyNc5Y1ozCTzO708yq0K80nm9zorf/n7U73yv/O70COLSAPIIxrNeHlM7bkRvbyq2gbZqD2/9raLR9lla7qvrY06kEf08vpaH8wnmd9pxsvrt+a++Pd0j7xWg9VefgQYrWH08s7znr3zvGePFXufPVbsffZY935GwLr38xWw7h4ft7H6qDQfww4AAAAASUVORK5CYII='
 
-let denoLogo
-
-const currentScene = {
-  async setup (canvas) {
-    let loadImage
-    if ('Deno' in window) {
-      loadImage = (await import('https://deno.land/x/canvas/mod.ts')).loadImage
-    } else {
-      loadImage = src => new Promise((resolve, reject) => {
-        const image = new Image()
-        image.onload = () => resolve(image)
-        image.onerror = reject
-        image.src = src
-      })
-    }
-    const image = await loadImage(spritesheet)
-    denoLogo = new Animation(image, [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15], 150, 24, 24)
-  },
-
-  draw (time, ctx) {
-    denoLogo.draw(ctx, time, 0, 0, 20, 20)
-  }
+async function main () {
+  const utils = new CanvasUtils()
+  const image = await utils.image(spritesheet)
+  const denoLogo = new Animation(image, [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15], 150, 24, 24)
+  const canvas = await utils.canvas(800, 800)
+  const context = canvas.getContext('2d')
+  context.imageSmoothingEnabled = false
+  utils.loop(() => denoLogo.draw(context, Date.now(), 0, 0, 40, 40), canvas)
 }
-
-async function mainDeno () {
-  const { createCanvas } = await import('https://deno.land/x/canvas/mod.ts')
-  const { MiniFB } = await import('../mod.ts')
-  const canvas = createCanvas(400, 400)
-  const ctx = canvas.getContext('2d')
-  const win = new MiniFB('minifb!', 400, 400)
-
-  setInterval(() => {
-    if (win.open) {
-      currentScene.draw(Date.now(), ctx)
-      win.updateWithBuffer(canvas.getRawBuffer(0, 0, 400, 400))
-      // win.update()
-    } else {
-      Deno.exit(0)
-    }
-  }, 1000 / 60)
-}
-
-async function mainBrowser () {
-  const canvas = document.getElementById('canvas')
-  const ctx = canvas.getContext('2d')
-  ctx.webkitImageSmoothingEnabled = false
-  ctx.mozImageSmoothingEnabled = false
-  ctx.imageSmoothingEnabled = false
-
-  const frame = () => {
-    currentScene.draw(Date.now(), ctx)
-    window.requestAnimationFrame(frame)
-  }
-
-  await currentScene.setup(canvas)
-  window.requestAnimationFrame(frame)
-}
-
-if ('Deno' in window) {
-  mainDeno()
-} else {
-  mainBrowser()
-}
+main()
